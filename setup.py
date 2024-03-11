a5d459d0a3b09293ed9634807439dd2c2a0eb060
housszak <48485225+HoussamZak@users.noreply.github.com>
2024-03-10 20:20:47 -0400

    minor fix

--- setup.py
+++ setup.py
@@ -8,11 +8,6 @@ setup(
     setup_requires = ['pytest-runner'],
     tests_require = ['pytest==4.4.1'],
     test_suite = 'tests',
-    packages=find_packages(),
-    classifiers=[
-    'Programming Language :: Python :: 3',
-    'License :: OSI Approved :: MIT License',
-    'Operating System :: OS Independent',
-    ],
+    packages=find_packages(),,
     python_requires='>=3.8',
 )
